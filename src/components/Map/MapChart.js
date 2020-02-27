5093c726ae11a887892ebaff3a7dc7fc2015e608
Yu Lin <linyu3294@gmail.com>
2020-02-27 17:32:50 -0500

    fixed the layout a little

--- src/components/Map/MapChart.js
+++ src/components/Map/MapChart.js
@@ -5,35 +5,45 @@ import {Spring} from 'react-spring/renderprops'
 import chroma from "chroma-js"
 import Countries from "./Countries";
 import projection from "d3-geo/src/projection";
+import {geoTimes} from "d3-geo-projection";
 
 
 //useZoomPan
 const geoUrl =
     // "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-"/world.json"
+    "/world.json"
 
 const getRandomInt = (min, max) =>
     Math.floor(Math.random() * (max - min + 1) + min)
 
 
 const colorScale = chroma.brewer.Oranges.slice(1)
+
 const colors = Array(180)
     .fill()
     .map(d => colorScale[getRandomInt(0, colorScale.length - 1)])
 
-class MapChart extends Component {
+const defaultCenter = [4,-2]
 
+class MapChart extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            width: 500,
+            height: 700,
             detail: false,
             paths: geoUrl,
-            center: [0, 0],
+            center: defaultCenter,
             zoom: 1
         }
+        this.projection = this.projection.bind(this)
     }
-
+    projection() {
+        return geoTimes()
+            .translate([this.state.width/2, this.state.height/2])
+            .scale(180)
+    }
     // ZoomControl=()=> {
     //     (this.props) = useSpring({zoom: 2, from: {zoom: 1}})
     // }
@@ -41,8 +51,8 @@ class MapChart extends Component {
         const { detail } = this.state;
         this.setState({
             paths: detail ? geoUrl : '/ch.json',
-            center: detail ? [0, 0] : [8, 47],
-            zoom: detail ? 1 : 60,
+            center: detail ? defaultCenter : [8, 47],
+            zoom: detail ? 1 : 20,
             detail: !detail
         });
     };
@@ -52,27 +62,30 @@ class MapChart extends Component {
             <Spring
                 from={{ zoom: 1 }}
                 to={{ zoom: this.state.zoom }}
+
             >
                 {styles =>(
                     <div>
-                    {/*<animated.div style={zoomIn}>I will fade in</animated.div>*/}
-                    <ComposableMap>
-                    <ZoomableGroup center={this.state.center} zoom={styles.zoom}>
-                    <Geographies geography={geoUrl}>
-                        {({geographies, proj}) =>
-                            geographies.map((geo,iter) =>
-                                <Countries
-                                geo = {geo}
-                                iter ={iter}
-                                proj = {proj}
-                                colors = {colors}
-                                switchPaths={this.switchPaths}
-                                />
-                        )}
-                    </Geographies>
-                    </ZoomableGroup>
-                    </ComposableMap>
-                </div>
+                        {/*<animated.div style={zoomIn}>I will fade in</animated.div>*/}
+                        <ComposableMap       width={this.props.width}
+                                             height={this.props.height}
+                                             projection={this.projection()}>
+                            <ZoomableGroup   center={defaultCenter} zoom={styles.zoom}>
+                                <Geographies geography={geoUrl}>
+                                    {({geographies, projection}) =>
+                                        geographies.map((geo,iter) =>
+                                            <Countries
+                                                geo = {geo}
+                                                iter ={iter}
+                                                projection = {projection}
+                                                colors = {colors}
+                                                switchPaths={this.switchPaths}
+                                            />
+                                        )}
+                                </Geographies>
+                            </ZoomableGroup>
+                        </ComposableMap>
+                    </div>
                 )}
             </Spring>
         )
